69e4c1c2b78f0c57d8ae0dddff64d5fe2344c37c
N0I0C0K <nick131410@aliyun.com>
2022-01-02 10:51:02 +0800

    Copy from my PowerToys fork
    project structure from N0I0C0K/PowerTranslator

---
+++ src/DictionarySearchResult.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+// using System;
+namespace Community.PowerToys.Run.Plugin.Dictionary
+{
+    public class DictionarySearchResult
+    {
+        public string KeyString { get; }
+
+        public string ResultString { get; }
+
+        public int Score { get; }
+
+        public DictionarySearchResult(string keyString, string resultString, int score)
+        {
+            KeyString = keyString;
+            ResultString = resultString;
+            Score = score;
+        }
+
+        /*public override bool Equals(object obj)
+        {
+            return obj is DictionarySearchResult result &&
+                   KeyString == result.KeyString &&
+                   ResultString == result.ResultString;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(KeyString, ResultString);
+        }*/
+    }
+}
---
+++ src/Main.cs
@@ -0,0 +1,190 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows;
+using System.Windows.Input;
+using ManagedCommon;
+using Wox.Plugin;
+
+namespace Community.PowerToys.Run.Plugin.Dictionary
+{
+    public class Main : IPlugin, IPluginI18n, IContextMenu, IDisposable
+    {
+        public string Name => Properties.Resources.plugin_name;
+
+        public string Description => Properties.Resources.plugin_description;
+
+        private PluginInitContext _context;
+        private static string _icon_path;
+        private bool _disposed;
+
+        private InputInterpreter _inputInterpreter;
+
+        public void Init(PluginInitContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(context));
+            }
+
+            _context = context;
+            _context.API.ThemeChanged += OnThemeChanged;
+            UpdateIconPath(_context.API.GetCurrentTheme());
+
+            _inputInterpreter = new InputInterpreter();
+        }
+
+        public List<Result> Query(Query query)
+        {
+            if (query == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(query));
+            }
+
+            // Parse
+            List<DictionarySearchResult> resultList = _inputInterpreter.QueryDictionary(query.Search);
+            if (resultList?.Any() != true)
+            {
+                return new List<Result>();
+            }
+
+            // Convert
+            return resultList
+              .Select(str => GetResult(str))
+              .ToList();
+        }
+
+        private Result GetResult(DictionarySearchResult result)
+        {
+            return new Result
+            {
+                ContextData = result,
+                Title = string.Format("{0}: {1}", result.KeyString, result.ResultString),
+                IcoPath = _icon_path,
+                Score = result.Score,
+                SubTitle = string.Format(Properties.Resources.copy_to_clipboard, result.ResultString),
+                Action = c =>
+                {
+                    var ret = false;
+                    var thread = new Thread(() =>
+                    {
+                        try
+                        {
+                            Clipboard.SetText(result.ResultString);
+                            ret = true;
+                        }
+                        catch (ExternalException)
+                        {
+                            MessageBox.Show(Properties.Resources.copy_failed);
+                        }
+                    });
+                    thread.SetApartmentState(ApartmentState.STA);
+                    thread.Start();
+                    thread.Join();
+                    return ret;
+                },
+            };
+        }
+
+        private ContextMenuResult CreateContextMenuEntry(DictionarySearchResult result)
+        {
+            return new ContextMenuResult
+            {
+                PluginName = Name,
+                Title = Properties.Resources.context_menu_copy,
+                Glyph = "\xE8C8",
+                FontFamily = "Segoe MDL2 Assets",
+                AcceleratorKey = Key.Enter,
+                Action = _ =>
+                {
+                    bool ret = false;
+                    var thread = new Thread(() =>
+                    {
+                        try
+                        {
+                            Clipboard.SetText(result.ResultString);
+                            ret = true;
+                        }
+                        catch (ExternalException)
+                        {
+                            MessageBox.Show(Properties.Resources.copy_failed);
+                        }
+                    });
+                    thread.SetApartmentState(ApartmentState.STA);
+                    thread.Start();
+                    thread.Join();
+                    return ret;
+                },
+            };
+        }
+
+        public List<ContextMenuResult> LoadContextMenus(Result selectedResult)
+        {
+            if (!(selectedResult?.ContextData is DictionarySearchResult))
+            {
+                return new List<ContextMenuResult>();
+            }
+
+            List<ContextMenuResult> contextResults = new List<ContextMenuResult>();
+            DictionarySearchResult result = selectedResult.ContextData as DictionarySearchResult;
+            contextResults.Add(CreateContextMenuEntry(result));
+
+            return contextResults;
+        }
+
+        public string GetTranslatedPluginTitle()
+        {
+            return Properties.Resources.plugin_name;
+        }
+
+        public string GetTranslatedPluginDescription()
+        {
+            return Properties.Resources.plugin_description;
+        }
+
+        private void OnThemeChanged(Theme currentTheme, Theme newTheme)
+        {
+            UpdateIconPath(newTheme);
+        }
+
+        private static void UpdateIconPath(Theme theme)
+        {
+            if (theme == Theme.Light || theme == Theme.HighContrastWhite)
+            {
+                _icon_path = "Images/dictionary.light.png";
+            }
+            else
+            {
+                _icon_path = "Images/dictionary.dark.png";
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    if (_context != null && _context.API != null)
+                    {
+                        _context.API.ThemeChanged -= OnThemeChanged;
+                    }
+
+                    _disposed = true;
+                }
+            }
+        }
+    }
+}
