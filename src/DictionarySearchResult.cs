f3ed1b9efa83602ea5db2161284d2867c18555c3
voruti <45130096+voruti@users.noreply.github.com>
2023-03-09 22:10:09 +0100

    Format

--- src/DictionarySearchResult.cs
+++ src/DictionarySearchResult.cs
@@ -2,21 +2,20 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-namespace Dictionary
+namespace Dictionary;
+
+public class DictionarySearchResult
 {
-    public class DictionarySearchResult
+    public DictionarySearchResult(string keyString, string resultString, int score)
     {
-        public string KeyString { get; }
-
-        public string ResultString { get; }
-
-        public int Score { get; }
-
-        public DictionarySearchResult(string keyString, string resultString, int score)
-        {
-            KeyString = keyString;
-            ResultString = resultString;
-            Score = score;
-        }
+        KeyString = keyString;
+        ResultString = resultString;
+        Score = score;
     }
+
+    public string KeyString { get; }
+
+    public string ResultString { get; }
+
+    public int Score { get; }
 }
